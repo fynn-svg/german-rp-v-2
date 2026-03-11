68c8dd532d1dee801bed58fe17139bf44a9777ea
Fynns <Fynn.schaltegger@gmail.com>
2026-03-11 15:11:12 +0100

    Update Webseite – z.B. Login und Styles geändert

--- script.js
+++ script.js
@@ -1,21 +1,22 @@
+// Bewerben Button
 function bewerben(){
-window.location.href="https://teams.galaxybot.app/application/1475499329723826198";
+  window.location.href="https://teams.galaxybot.app/application/1475499329723826198";
 }
 
+// Zur Webseite Button
 function zurWebseite(){
-alert("Webseite wird bald verfügbar sein!");
+  alert("Webseite wird bald verfügbar sein!");
 }
 
+// Login Funktion (Test)
 function login(){
+  let user = document.getElementById("username").value;
+  let pass = document.getElementById("password").value;
 
-let user = document.getElementById("username").value;
-let pass = document.getElementById("password").value;
-
-if(user === "Owner" && pass === "1234"){
-document.getElementById("loginstatus").innerText = "Login erfolgreich!";
-}
-else{
-document.getElementById("loginstatus").innerText = "Falsche Login Daten!";
-}
-
+  // Test-Benutzer: Owner / 1234
+  if(user === "Owner" && pass === "1234"){
+    document.getElementById("loginstatus").innerText = "Login erfolgreich!";
+  } else {
+    document.getElementById("loginstatus").innerText = "Falsche Login Daten!";
+  }
 }
